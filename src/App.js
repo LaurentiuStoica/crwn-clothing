e03dd51663aea535023ca934fdbc652367bca011
Laurentiu Stoica <stoica363@gmail.com>
2020-07-20 12:16:33 +0300

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included withSpinner HOC

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ class App extends Component {
         });
       } else {
         setCurrentUser(userAuth);
+        // addCollectionAndDocuments(
+        //   "collections",
+        //   collectionsArray.map(({ title, items }) => ({ title, items }))
+        // );
       }
     });
   }
