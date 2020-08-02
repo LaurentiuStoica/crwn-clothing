52e2ae6e2fdf51e9be7940a756a46bcbe8b4dadb
Laurentiu Stoica <stoica363@gmail.com>
2020-08-02 15:33:24 +0300

    added fix on server side

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const onToken = (token) => {
-    alert("Payment Succesful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForSripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card."
+        );
+      });
   };
 
   const priceForSripe = price * 100;
-  asd;
   const publishableKey =
     "pk_test_51H4UGcJTsJQmDMTh7GFpaAn6LrThS33u8VJ0lJe1ca2JsPxXlWx34Qk9QZd5PAGzl5dTioLc4dw1vqTr83QPpIZn005IOciMbq";
   return (
